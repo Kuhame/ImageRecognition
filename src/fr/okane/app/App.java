ce9f4b829821b9829259b8d812bc51cbd75b74ff
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-13 19:23:40 +0100

    Add RGB histogram

--- src/fr/okane/app/App.java
+++ src/fr/okane/app/App.java
@@ -1,17 +1,20 @@
 package fr.okane.app;
 
-import fr.okane.utils.ImageFilter;
-import fr.okane.utils.NoiseTools;
+import fr.okane.histogram.Histogram;
 import fr.unistra.pelican.Image;
 import fr.unistra.pelican.algorithms.io.ImageLoader;
 import fr.unistra.pelican.algorithms.visualisation.Viewer2D;
 
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
-        Image image = NoiseTools.addNoise(ImageLoader.exec("./img/tmp/eiffel.jpg"), 0.05);
+    public static void main(String[] args) throws IOException {
+        /*Image image = NoiseTools.addNoise(ImageLoader.exec("./img/tmp/eiffel.jpg"), 0.05);
         Viewer2D.exec(image);
         image = ImageFilter.median(image);
-        afficherRGB(image);
+        afficherRGB(image);*/
+
+        Histogram.RGB(ImageLoader.exec("./img/tmp/sobRGB.png"));
     }
 
     public static void afficherRGB(Image image) {
--- src/fr/okane/histogram/Histogram.java
+++ src/fr/okane/histogram/Histogram.java
@@ -1,4 +1,48 @@
 package fr.okane.histogram;
 
+import fr.okane.utils.HistogramTools;
+import fr.unistra.pelican.Image;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Histogram {
+    /**
+     * Construit et affiche l'histogramme d'une image à niveaux de gris
+     *
+     * @param image L'image dont on veut construire l'histogramme
+     */
+    public static double[] histogramme(Image image, int canal) {
+        double[] histogramme = new double[256];
+        Arrays.fill(histogramme, 0);
+
+        int largeur = image.getXDim();
+        int hauteur = image.getYDim();
+
+        for (int x = 0; x < largeur; ++x) {
+            for (int y = 0; y < hauteur; ++y) {
+                ++histogramme[image.getPixelXYBByte(x, y, canal)];
+            }
+        }
+
+        //HistogramTools.plotHistogram(h);
+        return histogramme;
+    }
+
+    /**
+     * Construit et affiche l'histogramme d'une image RGB
+     *
+     * @param image L'image RGB dont on veut construire l'histogramme
+     */
+    public static double[][] RGB(Image image) throws IOException {
+        double[][] h = new double[3][256];
+
+        for (int canal = 0; canal < 3; ++canal) {
+            double[] histCanal = histogramme(image, canal);
+            System.arraycopy(histCanal, 0, h[canal], 0, 256);
+            HistogramTools.plotHistogram(h[canal]);
+        }
+
+        return h;
+    }
 }
