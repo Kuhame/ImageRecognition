3295494af78bc5e78209f2019f23b485865608ca
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-13 18:23:07 +0100

    Test median filter

--- src/fr/okane/app/App.java
+++ src/fr/okane/app/App.java
@@ -1,7 +1,21 @@
 package fr.okane.app;
 
+import fr.okane.utils.ImageFilter;
+import fr.okane.utils.NoiseTools;
+import fr.unistra.pelican.Image;
+import fr.unistra.pelican.algorithms.io.ImageLoader;
+import fr.unistra.pelican.algorithms.visualisation.Viewer2D;
+
 public class App {
     public static void main(String[] args) {
-        
+        Image image = NoiseTools.addNoise(ImageLoader.exec("./img/tmp/eiffel.jpg"), 0.05);
+        Viewer2D.exec(image);
+        image = ImageFilter.median(image);
+        afficherRGB(image);
+    }
+
+    public static void afficherRGB(Image image) {
+        image.setColor(true);
+        Viewer2D.exec(image);
     }
 }
